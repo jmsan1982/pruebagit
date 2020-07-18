0145adef1dbecfbf44ffe42162198ebbecc2deaf
jose <jmsanchez1982@gmail.com>
2020-07-18 12:22:40 +0200

    commit chema

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -95,6 +95,7 @@
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
+            <p>primera linea chema</p>
         </div>
     </body>
 </html>
